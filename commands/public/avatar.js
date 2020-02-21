5937f7d6bd21bef343f09eee93f7cc03eab66fc7
DevShaded <devshaded@gmail.com>
2020-02-21 17:03:29 +0100

    edit: cleaned the code

--- commands/public/avatar.js
+++ commands/public/avatar.js
@@ -1,30 +1,30 @@
 'use strict';
 
 module.exports = {
-    name: 'avatar',
-    description: 'displays a picture of a mentioned users avatar',
-    execute: async function (client, message, args){
+  name: 'avatar',
+  description: 'displays a picture of a mentioned users avatar',
+  execute: async function (client, message, args){
 
-        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-        if (member){
-            message.channel.send({
-                embed: {
-                    color:'#FF6000',
-                    image: {
-                        url: member.user.displayAvatarURL(),                  
-                    },
-                }
-            });   
-        } else {
-            message.channel.send({
-                embed: {
-                    color:'#FF6000',
-                    image: {
-                        url: message.author.displayAvatarURL(),                  
-                    },
-                }
-            });   
+    if (member){
+      message.channel.send({
+        embed: {
+          color:'#FF6000',
+          image: {
+            url: member.user.displayAvatarURL(),
+          },
         }
-    }        
+      });
+    } else {
+      message.channel.send({
+        embed: {
+          color:'#FF6000',
+          image: {
+          url: message.author.displayAvatarURL(),
+          },
+        }
+      });
+    }
+  }
 };
